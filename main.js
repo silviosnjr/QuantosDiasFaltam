9647cc145a99200057da91615a83f559237967cd
Silvio Sales do Nascimento Junior <silviosnjr@gmail.com>
2024-04-01 20:14:20 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ const dataAtual = new Date();
 let diasQueFaltam = calculaDias();
 
 while(diasQueFaltam < 0){
-    alert("A data informada é menor que a data atual, atualize a página e insira uma nova data");
+    alert("A data informada é menor que a data atual, insira uma nova data");
     diasQueFaltam = calculaDias();
 }
 document.querySelector("#dias_restantes").textContent = diasQueFaltam+" dias";
 
 function calculaDias(){
-    let dataObjetivo = prompt("Data final (formato ANO-MÊS-DIA, exemplo: 2024-05-30");
+    let dataObjetivo = prompt("Data final (formato ANO-MÊS-DIA, exemplo: 2024-05-30):");
     dataObjetivo = new Date(dataObjetivo+"T23:59:59");
     return Math.floor((dataObjetivo - dataAtual) / 86400000);
 }
